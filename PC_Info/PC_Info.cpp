4f916701b4fba5e11da31a87db5ec98082d1bc23
David Hancock <dhancock@outlook.com>
2017-07-19 14:23:58 -0400

    Took out placeholder "Hello World" print and added two system calls - GetSystemInfo and GetVersionEx.

--- PC_Info/PC_Info.cpp
+++ PC_Info/PC_Info.cpp
@@ -3,14 +3,39 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include "windows.h"
 
 int main()
 {
-	std::cout << "Hello, world!\n";
+
+	SYSTEM_INFO info;
+	GetSystemInfo(&info);
+	std::cout << "dwPageSize = " << info.dwPageSize << "\n" << "\n";
+	std::cout << "lpMinimumApplicationAddress = " << info.lpMinimumApplicationAddress << "\n";
+	std::cout << "lpMaximumApplicationAddress = " << info.lpMaximumApplicationAddress << "\n";
+	std::cout << "dwActiveProcessorMask = " << info.dwActiveProcessorMask << "\n";
+	std::cout << "dwNumberOfProcessors = " << info.dwNumberOfProcessors << "\n";
+	std::cout << "dwProcessorType = " << info.dwProcessorType << "\n";
+	std::cout << "dwAllocationGranularity = " << info.dwAllocationGranularity << "\n";
+	std::cout << "wProcessorLevel = " << info.wProcessorLevel << "\n";
+	std::cout << "wProcessorRevision = " << info.wProcessorRevision << "\n";
+
+	OSVERSIONINFO osvi;
+	osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	GetVersionEx(&osvi);
+
+	std::cout << "\n";
+
+	std::cout << "dwOSVersionInfoSize = " << osvi.dwOSVersionInfoSize << "\n";
+	std::cout << "dwMajorVersion = " << osvi.dwMajorVersion << "\n";
+	std::cout << "dwMinorVersion = " << osvi.dwMinorVersion << "\n";
+	std::cout << "dwBuildNumber = " << osvi.dwBuildNumber << "\n";
+	std::cout << "dwPlatformId = " << osvi.dwPlatformId << "\n";
+	std::cout << "szCSDVersion = " << osvi.szCSDVersion << "\n";
 
 	int x;
 	std::cin >> x;
 
-    return 0;
+	return 0;
 }
 
